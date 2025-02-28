601776c1befc7bc6e4411b565bbc25f6e6928520
athenova <aleksey.sergeyev@yandex.com>
2025-02-28 21:07:30 +0300

    Рефакторинг, переход на DeepSeek

--- horo.py
+++ horo.py
@@ -3,51 +3,49 @@ import telebot
 import schedule
 import time
 import locale
+from openai import OpenAI
 from datetime import datetime
 from datetime import timedelta
-from openai import OpenAI
 
-AI_TEXT_MODEL = 'gpt-4o-mini'
+AI_TEXT_MODEL = 'deepseek-chat'
+CHAT_TOKEN_NAME = "DEEPSEEK_API_KEY"
 BOT_TOKEN_NAME = "ATHE_BOT_TOKEN"
 BOT_TOKEN = os.environ.get(BOT_TOKEN_NAME)
 #CHAT_ID = -1002374309134
-CHAT_ID = '@horo_ai'
+CHAT_ID = '@ai_tarot'
 
-def job(sign, symbol, month):
-    client = OpenAI()
+def job(sign, CHAT_ID=CHAT_ID):
+    locale.setlocale(locale.LC_ALL, ('ru_RU', 'UTF-8'))
+    date = datetime.now() + timedelta(days=10)
+    month = date.strftime('%B')
+    year = date.year
+    client = OpenAI(api_key=os.environ.get(CHAT_TOKEN_NAME), base_url="https://api.deepseek.com")
     text = client.chat.completions.create(
         model=AI_TEXT_MODEL,
         messages=[
             { "role": "system", "content": f"Ты - профессиональный астролог" },
-            { "role": "user", "content": f"Составь гороскоп на {month} для знака '{sign}', используй смайлики, не пиши дату" },
+            { "role": "user", "content": f"Составь гороскоп на {month} {year} год для знака '{sign}', используй смайлики, используй не более 300 слов" },
         ]
     ).choices[0].message.content
     bot = telebot.TeleBot(BOT_TOKEN)
-    text = f"""{symbol} **{sign}** {symbol}
-
-{text}    
-"""
     bot.send_message(chat_id=CHAT_ID, text=text, parse_mode="Markdown")
 
-locale.setlocale(locale.LC_ALL, ('ru_RU', 'UTF-8'))
-date = datetime.now() + timedelta(days=10)
-month = date.strftime('%B')
+if __name__ == '__main__':
+    schedule.every().day.at("19:00",'Europe/Moscow').do(job, sign="Рыбы", CHAT_ID='@pisces_the')
+    schedule.every().day.at("19:01",'Europe/Moscow').do(job, sign="Овен", CHAT_ID='@aries_the')
+    schedule.every().day.at("19:02",'Europe/Moscow').do(job, sign="Телец")
+    schedule.every().day.at("19:03",'Europe/Moscow').do(job, sign="Близнецы", CHAT_ID='@gemini_the')
+    schedule.every().day.at("19:04",'Europe/Moscow').do(job, sign="Рак")
+    schedule.every().day.at("19:05",'Europe/Moscow').do(job, sign="Лев")
+    schedule.every().day.at("19:06",'Europe/Moscow').do(job, sign="Дева")
+    schedule.every().day.at("19:07",'Europe/Moscow').do(job, sign="Весы")
+    schedule.every().day.at("19:08",'Europe/Moscow').do(job, sign="Скорпион")
+    schedule.every().day.at("19:09",'Europe/Moscow').do(job, sign="Стрелец")
+    schedule.every().day.at("19:10",'Europe/Moscow').do(job, sign="Козерог", CHAT_ID='@capricorn_the')
+    schedule.every().day.at("19:11",'Europe/Moscow').do(job, sign="Водолей", CHAT_ID='@aquarius_the')
 
-schedule.every().day.at("16:00",'Europe/Moscow').do(job, sign="Рыбы", symbol='♓', month=month)
-schedule.every().day.at("16:01",'Europe/Moscow').do(job, sign="Овен", symbol='♈', month=month)
-schedule.every().day.at("16:02",'Europe/Moscow').do(job, sign="Телец", symbol='♉', month=month)
-schedule.every().day.at("16:03",'Europe/Moscow').do(job, sign="Близнецы", symbol='♊', month=month)
-schedule.every().day.at("16:04",'Europe/Moscow').do(job, sign="Рак", symbol='♋', month=month)
-schedule.every().day.at("16:05",'Europe/Moscow').do(job, sign="Лев", symbol='♌', month=month)
-schedule.every().day.at("16:06",'Europe/Moscow').do(job, sign="Дева", symbol='♍', month=month)
-schedule.every().day.at("16:07",'Europe/Moscow').do(job, sign="Весы", symbol='♎', month=month)
-schedule.every().day.at("16:08",'Europe/Moscow').do(job, sign="Скорпион", symbol='♏', month=month)
-schedule.every().day.at("16:09",'Europe/Moscow').do(job, sign="Стрелец", symbol='♐', month=month)
-schedule.every().day.at("16:10",'Europe/Moscow').do(job, sign="Козерог", symbol='♑', month=month)
-schedule.every().day.at("16:11",'Europe/Moscow').do(job, sign="Водолей", symbol='♒', month=month)
+    fifteen_minutes = 15 * 60
 
-fifteen_minutes = 15 * 60
-
-for i in range(fifteen_minutes):
-    schedule.run_pending()
-    time.sleep(1)
+    for i in range(fifteen_minutes):
+        schedule.run_pending()
+        time.sleep(1)
---
+++ review.py
@@ -0,0 +1,5 @@
+from horo import job
+
+CHAT_ID = -1002374309134
+
+job('Рыбы', CHAT_ID=CHAT_ID)
